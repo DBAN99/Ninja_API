79cc30089659e570524463a17ec159c0c9b9ef07
DBAN99 <d.ban7171@gmail.com>
2022-01-30 17:55:30 +0900

    Feat : 장고 닌자 가이드 따라잡기 1일차
    장고 닌자 공식 문서 가이드라인 따라하는 중
    
    FastAPI와 유사하면서도 장고의 기본 폼으로도 빠른 API를 만들 수 있다는 점이 흥미로움
    
    Swagger를 사용한다는 점과 기존 DRF와는 훨씬 코드가 깔끔하다는 점이 맘에 듬

--- apidemo/apidemo/api.py
+++ apidemo/apidemo/api.py
@@ -1,23 +1,60 @@
-from ninja import NinjaAPI
+from typing import List
+from ninja import NinjaAPI, Query
 from ninja import Schema, Path
-
-import datetime
+from datetime import date
+from pydantic import Field
 
 api = NinjaAPI()
 
 
+@api.get("/items/{item_id}")
+def read_item(request, item_id : int):
+    return {"item_id": item_id}
+
+# ------------------------------------------------------
 class PathDate(Schema):
     year: int
     month: int
     day: int
 
     def value(self):
-        return datetime.date(self.year, self.month, self.day)
-
-@api.get("/items/{item_id}")
-def read_item(request, item_id : int):
-    return {"item_id": item_id}
+        return date(self.year, self.month, self.day)
 
 @api.get("/events/{year}/{month}/{day}")
 def events(request, date: PathDate = Path(...)):
     return {"date": date.value()}
+# ------------------------------------------------------
+
+
+weapons = ["Ninjato", "Shuriken", "Katana", "Kama", "Kunai", "Naginata", "Yari"]
+
+@api.get("/weapons")
+def list_weapons(request, limit: int = 10, offset: int = 0):
+    return weapons[offset: offset + limit]
+
+@api.get("/weapons/search")
+def search_weapons(request, q: str, offset: int = 0):
+    results = [w for w in weapons if q in w.lower()]
+    print(q, results)
+    return results[offset: offset + 10]
+
+# ------------------------------------------------------
+
+@api.get("/example")
+def example(request, s: str = None, b: bool = None, d: date = None, i: int = None):
+    return [s, b, d, i]
+
+# ------------------------------------------------------
+
+class Filters(Schema):
+    limit: int = 100
+    offset: int = None
+    query: str = None
+    category__in: List[str] = Field(None, alias="categories")
+
+
+@api.get("/filter")
+def events(request, filters: Filters = Query(...)):
+    return {"filters": filters.dict()}
+
+# ------------------------------------------------------
