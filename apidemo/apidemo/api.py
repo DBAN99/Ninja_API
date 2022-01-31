40c612eeaebd1f4f34b3d99272d1a2568938e9cf
DBAN99 <d.ban7171@gmail.com>
2022-01-31 19:27:02 +0900

    Feat : Request Body
    장고 닌자 공식 문서 따라잡기 중

--- apidemo/apidemo/api.py
+++ apidemo/apidemo/api.py
@@ -58,3 +58,18 @@ def events(request, filters: Filters = Query(...)):
     return {"filters": filters.dict()}
 
 # ------------------------------------------------------
+
+class Item(Schema):
+    name: str
+    description: str = None
+    price: float
+    quantity: int
+
+
+@api.post("/items")
+def create(request, item: Item):
+    return item
+
+@api.post("/items/{item_id}")
+def update(request, item_id: int, item: Item, q: str):
+    return {"item_id": item_id, "item": item.dict(), "q": q}
