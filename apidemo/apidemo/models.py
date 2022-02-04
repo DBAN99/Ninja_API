03f2bd0e6d236da33ec4826e8db33ea96c48ef5d
DBAN99 <d.ban7171@gmail.com>
2022-02-04 17:44:13 +0900

    Feat : Model 스키마
    스키마 설정하는 방법

--- apidemo/apidemo/models.py
+++ apidemo/apidemo/models.py
@@ -1,11 +1,29 @@
-from django.db import models
+from django.contrib.auth.models import User
+from ninja import ModelSchema
 
 
-class Department(models.Model):
-    title = models.CharField(max_length=100)
+# 특정 스키마 생성
+class A_UserSchema(ModelSchema):
+    class Config:
+        model = User
+        model_fields = ['id', 'username', 'first_name', 'last_name']
 
-class Employee(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    department = models.ForeignKey(Department)
-    birthdate = models.DateField(null=True, blank=True)
+# Will create schema like this:
+#
+# class UserSchema(Schema):
+#     id: int
+#     username: str
+#     first_name: str
+#     last_name: str
+
+# model_fields를 이용해 모든 column을 사용하는 방법 - 비추
+class B_UserSchema(ModelSchema):
+    class Config:
+        model = User
+        model_fields = "__all__"
+
+# model_exclude를 이용해서 사용하면 exclude에 해당하는 column을 제외하고 나머지 모두를 사용할 수 있음
+class C_UserSchema(ModelSchema):
+    class Config:
+        model = User
+        model_exclude = ['password', 'last_login', 'user_permissions']
